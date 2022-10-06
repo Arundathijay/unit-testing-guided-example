6447eb6bf01855961479df4d99c04d0cd8a0566e
Arundathi Jayarathne <sandyjayarathne@gmail.com>
2022-10-06 18:03:32 +0200

    tests practiced

--- tests/multiply.spec.js
+++ tests/multiply.spec.js
@@ -1,16 +1,18 @@
 const { multiply } = require('./../calculator.js');
+
 describe('Multiply', () => {
-  test('should divide two positive integers correctly', () => {
+  test('should multiply two positive integers correctly', () => {
     const result = multiply(1, 2);
     expect(result).toBe(2);
   });
 
-  test('should divide two floating point numbers correctly', () => {
-    const result = multiply(4.5, 2.5);
-    expect(result).toBe(11.25);
+  test('should multiply a negative and a positive number correctly', () => {
+    const result = multiply(-4.5, 2.5);
+    expect(result).toBe(-11.25);
   });
 
   test('should throw an error when multiplying by 0', () => {
-    expect(() => multiply(1, 0)).toThrow();
+    const result = multiply(1, 0);
+    expect(result).toBe(0);
   });
 });
